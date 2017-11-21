8f0ba74d990c1bc3a38fdf069cb5cd3784053ec6
ZazieTheBeast <kristofer.kass@gmail.com>
2017-11-21 17:19:20 -0500

    Added tag helpers and shopping cart

---
+++ DemoSite/ViewModels/HomeViewModel.cs
@@ -0,0 +1,13 @@
+﻿using DemoSite.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoSite.ViewModels
+{
+    public class HomeViewModel
+    {
+        public IEnumerable<Pie> PiesOfTheWeek { get; set; }
+    }
+}

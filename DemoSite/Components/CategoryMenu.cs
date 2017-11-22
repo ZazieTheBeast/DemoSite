c48d46b067bca97cb3e2fccd1f64a75b9b290d70
ZazieTheBeast <kristofer.kass@gmail.com>
2017-11-21 20:03:18 -0500

    added the top menu

---
+++ DemoSite/Components/CategoryMenu.cs
@@ -0,0 +1,24 @@
+﻿using DemoSite.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoSite.Components
+{
+    public class CategoryMenu : ViewComponent
+    {
+        private readonly ICategoryRepository _categoryRepository;
+        public CategoryMenu(ICategoryRepository categoryRepository)
+        {
+            _categoryRepository = categoryRepository;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var categories = _categoryRepository.Categories.OrderBy(c => c.CategoryName);
+            return View(categories);
+        }
+    }
+}
